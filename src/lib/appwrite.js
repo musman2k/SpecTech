c3678b0c57fb4274dfad5db1d19c83b48979f256
musman2k <usmanyousuf911@gmail.com>
2026-06-08 01:00:11 +0500

    src/lib/appwrite.js update

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,11 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, Databases, Functions } from "appwrite";
 
 const client = new Client()
   .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject("6a13522b0016a57b69ed");
 
-const account = new Account(client);
+const account   = new Account(client);
 const databases = new Databases(client);
+const functions = new Functions(client);
 
-export { client, account, databases };
+export { client, account, databases, functions };
